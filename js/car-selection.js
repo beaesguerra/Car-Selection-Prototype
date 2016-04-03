36e10bc62fe4e7ccac36236f411e2fc859d6d6e7
Bryson Ding <bryson189@gmail.com>
2016-04-03 16:48:20 -0600

    Finished build your car button

--- js/car-selection.js
+++ js/car-selection.js
@@ -10,8 +10,10 @@ $(document).ready(function() {
 	$("#main-image-text-2").fadeOut();
 	$("#main-image-text-3").fadeOut();
 	$("#main-image-text-4").fadeOut();
-	$("#main-image-text-5").fadeOut();
+	$("#main-image-text-5").fadeOut();	
+
   $('#thumbnail-1').on('click', function () {
+	$("#build-car-button").fadeOut(0);
 	$("#main-image-2").fadeOut();
 	$("#main-image-3").fadeOut();
 	$("#main-image-4").fadeOut();
@@ -23,8 +25,10 @@ $(document).ready(function() {
 	$("#main-image-text-4").fadeOut();
 	$("#main-image-text-5").fadeOut();
 	$("#main-image-text-1").delay(1000).fadeIn();
+	$("#build-car-button").delay(1500).fadeIn(50);
   });
   $('#thumbnail-2').on('click', function () {
+  	$("#build-car-button").fadeOut(0);
 	$("#main-image-1").fadeOut();
 	$("#main-image-3").fadeOut();
 	$("#main-image-4").fadeOut();
@@ -36,12 +40,14 @@ $(document).ready(function() {
 	$("#main-image-text-4").fadeOut();
 	$("#main-image-text-5").fadeOut();
 	$("#main-image-text-2").delay(1000).fadeIn();
+	$("#build-car-button").delay(1500).fadeIn(50);
   });
   $('#thumbnail-3').on('click', function () {
 	$("#main-image-1").fadeOut();
 	$("#main-image-2").fadeOut();
 	$("#main-image-4").fadeOut();
 	$("#main-image-5").fadeOut();
+	$("#build-car-button").fadeOut(0);
 	$("#main-image-3").delay(500).fadeIn();
 
 	$("#main-image-text-1").fadeOut();
@@ -49,12 +55,14 @@ $(document).ready(function() {
 	$("#main-image-text-4").fadeOut();
 	$("#main-image-text-5").fadeOut();
 	$("#main-image-text-3").delay(1000).fadeIn();
+	$("#build-car-button").delay(1500).fadeIn(50);
   });
   $('#thumbnail-4').on('click', function () {
 	$("#main-image-1").fadeOut();
 	$("#main-image-2").fadeOut();
 	$("#main-image-3").fadeOut();
 	$("#main-image-5").fadeOut();
+	$("#build-car-button").fadeOut(0);
 	$("#main-image-4").delay(500).fadeIn();
 
 	$("#main-image-text-2").fadeOut();
@@ -62,12 +70,14 @@ $(document).ready(function() {
 	$("#main-image-text-1").fadeOut();
 	$("#main-image-text-5").fadeOut();
 	$("#main-image-text-4").delay(1000).fadeIn();
+	$("#build-car-button").delay(1500).fadeIn(50);
   });
   $('#thumbnail-5').on('click', function () {
 	$("#main-image-1").fadeOut();
 	$("#main-image-2").fadeOut();
 	$("#main-image-3").fadeOut();
 	$("#main-image-4").fadeOut();
+	$("#build-car-button").fadeOut(0);
 	$("#main-image-5").delay(500).fadeIn();
 
 	$("#main-image-text-2").fadeOut();
@@ -75,5 +85,6 @@ $(document).ready(function() {
 	$("#main-image-text-4").fadeOut();
 	$("#main-image-text-1").fadeOut();
 	$("#main-image-text-5").delay(1000).fadeIn();
+	$("#build-car-button").delay(1500).fadeIn(50);
   });
 });
