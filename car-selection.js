e5f3ba5b3bbfb0347b34eb2e5fc96db2980c735d
Bryson Ding <bryson189@gmail.com>
2016-04-03 00:47:38 -0600

    Added basic text for car selection

--- car-selection.js
+++ car-selection.js
@@ -4,8 +4,6 @@ $(document).ready(function() {
 	$("#main-image-4").fadeOut();
 	$("#main-image-5").fadeOut();
 	$("#main-image-1").fadeIn();
-	// var mainImageText = document.getElementById("main-image-text");
- //    mainImageText.textContent = "Audi 951";
 
   $('#thumbnail-1').on('click', function () {
 	$("#main-image-2").fadeOut();
