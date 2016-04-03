dd5a06a8202f49f925da35e45216110c13e400a9
Bryson Ding <bryson189@gmail.com>
2016-04-02 23:16:27 -0600

    Added functionality to select different cars

--- js/car-selection.js
+++ js/car-selection.js
@@ -1,5 +1,37 @@
 $(document).ready(function() {
-  $('#myStateButton').on('click', function () {
-    $("#main-image-2").toggleClass('hidden');
-  })
+  $('#thumbnail-1').on('click', function () {
+	$("#main-image-1").removeClass('hidden');
+	$("#main-image-2").addClass('hidden');
+	$("#main-image-3").addClass('hidden');
+	$("#main-image-4").addClass('hidden');
+	$("#main-image-5").addClass('hidden');
+  });
+  $('#thumbnail-2').on('click', function () {
+	$("#main-image-2").removeClass('hidden');
+	$("#main-image-1").addClass('hidden');
+	$("#main-image-3").addClass('hidden');
+	$("#main-image-4").addClass('hidden');
+	$("#main-image-5").addClass('hidden');
+  });
+  $('#thumbnail-3').on('click', function () {
+	$("#main-image-3").removeClass('hidden');
+	$("#main-image-1").addClass('hidden');
+	$("#main-image-2").addClass('hidden');
+	$("#main-image-4").addClass('hidden');
+	$("#main-image-5").addClass('hidden');
+  });
+  $('#thumbnail-4').on('click', function () {
+	$("#main-image-4").removeClass('hidden');
+	$("#main-image-1").addClass('hidden');
+	$("#main-image-2").addClass('hidden');
+	$("#main-image-3").addClass('hidden');
+	$("#main-image-5").addClass('hidden');
+  });
+  $('#thumbnail-5').on('click', function () {
+	$("#main-image-5").removeClass('hidden');
+	$("#main-image-1").addClass('hidden');
+	$("#main-image-2").addClass('hidden');
+	$("#main-image-3").addClass('hidden');
+	$("#main-image-4").addClass('hidden');
+  });
 });
